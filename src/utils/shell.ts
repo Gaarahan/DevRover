185e2f27819e295ec7117282e20dc5dd38130b1d
Gaarahan <815199630@qq.com>
2024-02-06 14:43:02 +0800

    fix: add tmux env

--- src/utils/shell.ts
+++ src/utils/shell.ts
@@ -1,18 +1,20 @@
 import { exec } from "child_process";
 import { Action, ActionPanel, Clipboard, Form, Icon, showToast, Toast } from "@raycast/api";
 
+const BIN_ENV = Object.assign({}, process.env, { PATH: "/usr/local/bin:/usr/bin:/opt/homebrew/bin" });
+
 export const execCommand = async (command: string): Promise<string> => {
-    return new Promise((resolve, reject) => {
-      exec(command, (error, stdout, stderr) => {
-        if (error) {
-          reject(`Error: ${error.message}`);
-          return;
-        }
-        if (stderr) {
-          reject(`Stderr: ${stderr}`);
-          return;
-        }
-        resolve(stdout);
-      });
+  return new Promise((resolve, reject) => {
+    exec(command, { env: BIN_ENV }, (error, stdout, stderr) => {
+      if (error) {
+        reject(`Error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        reject(`Stderr: ${stderr}`);
+        return;
+      }
+      resolve(stdout);
     });
-  };
+  });
+};
