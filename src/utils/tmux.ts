d0ffea538cb7f5afd81458cdc33baedbd8869762
Gaarahan <815199630@qq.com>
2024-02-06 13:59:01 +0800

    fix: fix import

--- src/utils/tmux.ts
+++ src/utils/tmux.ts
@@ -1,4 +1,4 @@
-import { execCommand } from '.';
+import { execCommand } from './index';
 
 export function getAllSession() {
     return execCommand(`tmux list-sessions | awk '{print $1}' | sed 's/://'`)
